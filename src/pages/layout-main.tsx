94fd0e46fde24e5bb35110fbcb7cf7e38022e34f
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-10-09 13:41:59 -0300

    criando o header

--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import Text from "../components/text";
+import MainHeader from "../components/main-header";
 
 
 export default function LayoutMain() {
     return (
         <>
+            <MainHeader className="mt-9" />
             <Text variant="heading-large">Viva ao layout main</Text>
             <hr />
             <Outlet />
